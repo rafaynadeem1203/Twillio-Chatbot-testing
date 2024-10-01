d9893f402157bc33ca2f91cea0e27c002b99c9e6
rafaynadeem1203 <108399198+rafaynadeem1203@users.noreply.github.com>
2024-10-01 22:19:09 +0500

    Update workorders_utils.py

--- utils/workorders_utils.py
+++ utils/workorders_utils.py
@@ -6,13 +6,6 @@ from dateutil import parser
 import requests
 from utils.dbConfig import connect
 
-def format_date(iso_date):
-    if iso_date is None:
-        return iso_date
-    # Parse ISO date string to datetime object
-    date_obj = parser.isoparse(iso_date)
-    # Format to include date and time,"August 27, 2024, 07:00 PM"
-    return date_obj.strftime("%B %d, %Y, %I:%M %p") 
 
 def get_workorders(dbname):
     try:
@@ -23,9 +16,6 @@ def get_workorders(dbname):
         allWorkorders = list(user_collection.find({}))
         for item in allWorkorders:
             item['_id'] = str(item['_id'])
-            item['date']=format_date(item['date'])
-            item['startDate']=format_date(item['startDate'])
-            item['endDate']=format_date(item['endDate'])
 
         print("All workorders are: ", allWorkorders)    
         #return jsonify({allWorkorders})
@@ -51,9 +41,6 @@ def get_workorder_by_id(id,dbname):
         if workorder_details:
             print("Workorder details are: ", workorder_details)
             workorder_details["_id"]=str(workorder_details["_id"])
-            workorder_details['date']=format_date(workorder_details['date'])
-            workorder_details['startDate']=format_date(workorder_details['startDate'])
-            workorder_details['endDate']=format_date(workorder_details['endDate'])
             return workorder_details
         else:
             print("Workorder not found")
